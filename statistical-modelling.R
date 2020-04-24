e5ca627871d58fdeb4c961700f432628cefba376
Mukesh A <amukesh.mk@gmail.com>
2020-04-24 20:53:57 +0100

    added better filtering and fixed issue with tapply

--- statistical-modelling.R
+++ statistical-modelling.R
@@ -1,8 +1,7 @@
-rm(list = ls())
-
 # library imports
 library(ggplot2)
 library(MCMCpack)
+library(dplyr)
 
 # dataset is taken from https://www.kaggle.com/zynicide/wine-reviews
 
@@ -41,23 +40,15 @@ wine2_df = within(wine2_df, remove('province', 'region_1', 'region_2', 'winery')
 View(wine2_df)
 
 # combining both the data frames
-comb_wine_df <- rbind(wine_df, wine2_df)
+comb_wine_df = rbind(wine_df, wine2_df)
 View(comb_wine_df)
 
 # this filters the data as per Q1, Sauvignon Blanc Wine from South Africa, Priced at $15
-wine_sa_sb_15 <- comb_wine_df[comb_wine_df$country == "South Africa", ]
-wine_sa_sb_15 <- wine_sa_sb_15[wine_sa_sb_15$variety == "Sauvignon Blanc", ]
-wine_sa_sb_15 <- wine_sa_sb_15[wine_sa_sb_15$price == 15, ]
-wine_sa_sb_15 = within(wine_sa_sb_15, remove('country', 'price'))
-wine_sa_sb_15 = wine_sa_sb_15[rowSums(is.na(wine_sa_sb_15)) == 0, ]
+wine_sa_sb_15 = filter(comb_wine_df, variety=='Sauvignon Blanc' & country == 'South Africa' & price == 15)
 View(wine_sa_sb_15)
 
 # this filters the data as per Q1, Chardonnay Wine from Chile, Priced at $15
-wine_ch_ch_15 <- comb_wine_df[comb_wine_df$country == "Chile", ]
-wine_ch_ch_15 <- wine_ch_ch_15[wine_ch_ch_15$variety == "Chardonnay", ]
-wine_ch_ch_15 <- wine_ch_ch_15[wine_ch_ch_15$price == 15, ]
-wine_ch_ch_15 = within(wine_ch_ch_15, remove('country', 'price'))
-wine_ch_ch_15 = wine_ch_ch_15[complete.cases(wine_ch_ch_15), ]
+wine_ch_ch_15 = filter(comb_wine_df, variety=='Chardonnay' & country == 'Chile' & price == 15)
 View(wine_ch_ch_15)
 
 # combining the 2 data frames
@@ -66,10 +57,14 @@ comb_wine = rbind(wine_ch_ch_15, wine_sa_sb_15)
 ggplot(comb_wine) + geom_boxplot(aes(variety, points, fill = variety)) +
   geom_jitter(aes(variety, points, shape = variety))
 
+wine_means = tapply(comb_wine$points, comb_wine$variety, mean)
+wine_means[complete.cases(wine_means)]
 
-tapply(comb_wine$points, comb_wine$variety, mean)
-tapply(comb_wine$points, comb_wine$variety, median)
-tapply(comb_wine$points, comb_wine$variety, sd)
+wine_median = tapply(comb_wine$points, comb_wine$variety, median)
+wine_median[complete.cases(wine_median)]
+
+wine_sd = tapply(comb_wine$points, comb_wine$variety, sd)
+wine_sd[complete.cases(wine_sd)]
 
 t.test(points ~ variety, data=comb_wine, var.equal = TRUE)
 
