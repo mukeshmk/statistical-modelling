9b5ccf5930a97000185dcb17e778e0d97f5703a6
Mukesh A <amukesh.mk@gmail.com>
2020-04-24 02:05:44 +0100

    added possibily related code from hierarchical modelling case study

--- statistical-modelling.R
+++ statistical-modelling.R
@@ -72,3 +72,74 @@ tapply(comb_wine$points, comb_wine$variety, median)
 tapply(comb_wine$points, comb_wine$variety, sd)
 
 t.test(points ~ variety, data=comb_wine, var.equal = TRUE)
+
+
+compare_2_gibbs <- function(y, ind, mu0 = 50, tau0 = 1/400, del0 = 0, gamma0 = 1/400, 
+                            a0 = 1, b0 = 50, maxiter = 5000)
+{
+  y1 <- y[ind == 1]
+  y2 <- y[ind == 2]
+  
+  n1 <- length(y1) 
+  n2 <- length(y2)
+  
+  ##### starting values
+  mu <- (mean(y1) + mean(y2)) / 2
+  del <- (mean(y1) - mean(y2)) / 2
+  
+  mat_store <- matrix(0, nrow = maxiter, ncol = 3)
+  #####
+  
+  ##### Gibbs sampler
+  an <- a0 + (n1 + n2)/2
+  
+  for(s in 1 : maxiter) 
+  {
+    
+    ##update tau
+    bn <- b0 + 0.5 * (sum((y1 - mu - del) ^ 2) + sum((y2 - mu + del) ^ 2))
+    tau <- rgamma(1, an, bn)
+    ##
+    
+    ##update mu
+    taun <-  tau0 + tau * (n1 + n2)
+    mun <- (tau0 * mu0 + tau * (sum(y1 - del) + sum(y2 + del))) / taun
+    mu <- rnorm(1, mun, sqrt(1/taun))
+    ##
+    
+    ##update del
+    gamman <-  tau0 + tau*(n1 + n2)
+    deln <- ( del0 * tau0 + tau * (sum(y1 - mu) - sum(y2 - mu))) / gamman
+    del<-rnorm(1, deln, sqrt(1/gamman))
+    ##
+    
+    ## store parameter values
+    mat_store[s, ] <- c(mu, del, tau)
+  }
+  colnames(mat_store) <- c("mu", "del", "tau")
+  return(mat_store)
+}
+
+fit <- compare_2_gibbs(comb_wine$points, as.factor(comb_wine$variety))
+
+plot(as.mcmc(fit))
+
+raftery.diag(as.mcmc(fit))
+
+apply(fit, 2, mean)
+apply(fit, 2, sd)
+## easier to interpret standard deviation than precision
+mean(1/sqrt(fit[, 3])) 
+sd(1/sqrt(fit[, 3]))
+
+
+y1_sim <- rnorm(5000, fit[, 1] + fit[, 2], sd = 1/sqrt(fit[, 3]))
+y2_sim <- rnorm(5000, fit[, 1] - fit[, 2], sd = 1/sqrt(fit[, 3]))
+
+ggplot(data.frame(y_sim_diff = y1_sim - y2_sim)) + stat_bin(aes(y_sim_diff))
+
+mean(y1_sim > y2_sim)
+mean(y2_sim > y1_sim)
+
+ggplot(data.frame(y1_sim, y2_sim)) + geom_point(aes(y1_sim, y2_sim), alpha = 0.3) + 
+  geom_abline(slope = 1, intercept = 0)
