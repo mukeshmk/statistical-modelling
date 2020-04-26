0e4f09010ff4f30a4e7edd2df0f38e1276dbd9f7
Mukesh A <amukesh.mk@gmail.com>
2020-04-26 21:46:02 +0100

    updated code for mclust using new clustring results

--- statistical-modelling-2b.R
+++ statistical-modelling-2b.R
@@ -17,50 +17,52 @@ wine_df = within(wine_df, remove('X', 'description', 'designation', 'taster_name
 # these variables might be useful in the future, but ignorning them for now.
 wine_df = within(wine_df, remove('province', 'region_1', 'region_2', 'winery', 'variety'))
 
-# currently the data frame contains the following columns "country, points, price and variety" in this order
-# where X = {country, price and variety} and Y = points
+# currently the data frame contains the following columns "country, points and price" in this order
+# where X = {country and price} and Y = points
 View(wine_df)
 
-# this filters the data as per Q1b, Italian Wine, Priced less than $20 with regions which have at least 4 reviews
+# this filters the data as per Q2-b - US Wine
+# additionally filtering it to price less than 400 so as to avoid outliers
 wine_us = filter(wine_df, country == 'US' & price < 400)
 wine_us = na.omit(wine_us)
 wine_us = within(wine_us, remove('country'))
 View(wine_us)
 
+# plot "points vs price" for wine in US
 plot(wine_us)
 
-fit2 <- Mclust(wine_us)
+fit1 <- Mclust(wine_us)
 
-print(fit)
-summary(fit)
-plot(fit, what = "classification")
+print(fit1)
+summary(fit1)
+plot(fit1, what = "classification")
+plot(fit1, what = "BIC")
+fit1$BIC
 
+BIC <- mclustBIC(wine_us)
+plot(BIC)
+summary(BIC)
+
+fit2 <- Mclust(wine_us, G = 9, modelNames = "VVV")
 print(fit2)
 summary(fit2)
 plot(fit2, what = "classification")
-plot(fit2, what = "BIC")
-fit2$BIC
+plot(fit2, what = "uncertainty")
 
-fit3 <- Mclust(wine_us, G = 8, modelNames = "VVV")
+fit3 <- Mclust(wine_us, G = 9, modelNames = "VVE")
 print(fit3)
 summary(fit3)
 plot(fit3, what = "classification")
 plot(fit3, what = "uncertainty")
 
-fit4 <- Mclust(wine_us, G = 8, modelNames = "VVE")
+fit4 <- Mclust(wine_us, G = 8, modelNames = "VVV")
 print(fit4)
 summary(fit4)
 plot(fit4, what = "classification")
 plot(fit4, what = "uncertainty")
 
-fit5 <- Mclust(wine_us, G = 7, modelNames = "VVV")
+fit5 <- Mclust(wine_us, G = 8, modelNames = "VVV", prior = priorControl())
 print(fit5)
 summary(fit5)
 plot(fit5, what = "classification")
 plot(fit5, what = "uncertainty")
-
-fit6 <- Mclust(wine_us, G = 7, modelNames = "VVV", prior = priorControl())
-print(fit6)
-summary(fit6)
-plot(fit6, what = "classification")
-plot(fit6, what = "uncertainty")
